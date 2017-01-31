f8e87d496906ec7f258da8e24ae1273fa7ff967a
entcheva <christina.entcheva@gmail.com>
2017-01-31 17:49:21 -0500

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,48 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ];
 
+var OlderCoaster = React.createClass({
+  render: function() {
+    return (
+      React.createElement('div', {className: 'oldercoaster'}, [
+        React.createElement('p', {}, 'Two grannies having the time of their life!'),
+        React.createElement('p', {}, 'Passengers:'),
+        React.createElement('ul', {}, [
+          React.createElement('li', {}, 'Agnes'),
+          React.createElement('li', {}, 'Muriel')
+        ]), // end array: ul content & children
+    ]) // end array: div content & children
+    ) // end return
+  } // end render
+}) // end OlderCoaster
+
+var InFrontOfYou = React.createClass({
+  render: function() {
+    return (
+      React.createElement('div', {}, [
+        React.createElement('p', {}, 'You shouldn\'t look too far.'),
+        React.createElement('p', {}, [
+          'Sometimes, the solution is ',
+          React.createElement('strong', {}, 'right in front of you.')
+        ])
+      ]) // end array: div content & children
+    ) // end return
+  } // end render
+}) // end InFrontOfYou
+
+var ButcherShop = React.createClass({
+  render: function() {
+    return (
+      React.createElement('div', {className: 'butcher-shop'}, [
+        React.createElement('p', {}, 'Hello! We have the following products for sale today:'),
+        React.createElement('ul', {}, BUTCHER_PRODUCTS.map(item =>
+          React.createElement('li', {}, item))
+      ) // end array: ul content & children
+      ]) // end array: div content & children
+    ) // end return
+  } // end render
+}) // end ButcherShop
+
 ReactDOM.render(
   React.createElement('div', {}, [
     React.createElement(OlderCoaster),
